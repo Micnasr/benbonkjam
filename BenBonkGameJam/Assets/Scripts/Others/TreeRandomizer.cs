d2394821c95925484bc4ef6b00c81640ce1e5692
epersonf <epersoncmxf@gmail.com>
2020-05-30 18:01:28 -0300

    Spikes added and trees.

---
+++ BenBonkGameJam/Assets/Scripts/Others/TreeRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRandomizer : MonoBehaviour
+{
+    [SerializeField]
+    private List<Vector3> angles;
+
+    void Start()
+    {
+        int i = 0;
+        foreach (GameObject children in GameObject.FindGameObjectsWithTag("WoodBone"))
+        {
+            if (i >= angles.Count) break;
+            children.transform.eulerAngles = angles[i];
+            i++;
+        }
+    }
+}

f63dca5d352e6bfe2a9c8a972784902e59cfebb9
Micnasr <micraidz@gmail.com>
2020-05-29 20:05:49 -0400

    fadeinandout

---
+++ BenBonkGameJam/Assets/wincube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class wincube : MonoBehaviour
+{
+    public Animator animator;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(GoToNextLevel());
+    }
+    
+
+    public void FadeToLevel()
+    {
+        animator.SetTrigger("FadeOut");
+
+    }
+
+    IEnumerator GoToNextLevel()
+    {
+        FadeToLevel();
+        yield return new WaitForSeconds(.3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}

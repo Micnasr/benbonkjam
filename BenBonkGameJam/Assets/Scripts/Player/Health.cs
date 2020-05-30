029b02d6f0584ab0eaf70f1da4e2998884023ba8
RaideNnigth <32282947+RaideNnigth@users.noreply.github.com>
2020-05-30 14:03:22 -0300

    DeathImplementation

---
+++ BenBonkGameJam/Assets/Scripts/Player/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+
+    [Header("Characteristics")]
+    public float health;
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Die();
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        print("reload");
+        if (collision.tag == "EnemyBullet")
+        {
+            health -= 1;
+        }
+    }
+    
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}

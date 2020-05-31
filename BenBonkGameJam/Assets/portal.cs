420f6f15d3de4321366812ed9b6c471b808b8b27
Micnasr <micraidz@gmail.com>
2020-05-30 21:50:11 -0400

    e

---
+++ BenBonkGameJam/Assets/portal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour
+{
+    public GameObject point;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            collision.transform.position = point.transform.position;
+        }   
+    }
+}

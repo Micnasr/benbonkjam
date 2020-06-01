ee373e74a9b69cdad154bdeda774a1fbb0bc27f3
Micnasr <micraidz@gmail.com>
2020-05-31 20:51:51 -0400

    bugfix

--- BenBonkGameJam/Assets/Scripts/Others/StoryManager.cs
+++ BenBonkGameJam/Assets/Scripts/Others/StoryManager.cs
@@ -12,6 +12,8 @@ public class StoryManager : MonoBehaviour
     public GameObject Line2;
     public GameObject Line3;
 
+    public GameObject background;
+
     void Start()
     {
         StartCoroutine(wait());
@@ -19,6 +21,7 @@ public class StoryManager : MonoBehaviour
 
     IEnumerator wait()
     {
+        background.SetActive(true);
         Line1.SetActive(true);
         Line2.SetActive(false);
         Line3.SetActive(false);
@@ -34,6 +37,7 @@ public class StoryManager : MonoBehaviour
         Line1.SetActive(false);
         Line2.SetActive(false);
         Line3.SetActive(false);
+        background.SetActive(false);
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
